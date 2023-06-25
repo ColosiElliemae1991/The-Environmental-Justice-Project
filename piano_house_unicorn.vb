5af734a57852ac5a7bafb524634c227dc9a46b82
ColosiElliemae1991 <137498848+ColosiElliemae1991@users.noreply.github.com>
2023-06-25 17:32:36 +0700

    Improve error handling

---
+++ piano_house_unicorn.vb
@@ -0,0 +1,28 @@
+Public Class Environment
+
+Public Sub PromoteEquitableAccess()
+
+'Declare variables
+Dim envIneq As Boolean
+Dim healthEnvironments As Integer
+
+'Assign values
+envIneq = True
+healthEnvironments = 0
+
+'Check if there is environmental inequality
+If envIneq = True Then
+	
+	'Promote equitable access to healthy environments
+	While healthEnvironments < 2000
+		'Promote access
+		healthEnvironments = healthEnvironments + 1
+	Wend
+
+Else
+	'Do nothing
+End If
+
+End Sub
+
+End Class
